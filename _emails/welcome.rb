3db5a0a19221b1b1b8eeb4baf18d6ef9376f5e64
Drew Meyers <drew@ohheyworld.com>
2015-03-07 13:20:19 -0800

    draft one of welcome email

--- _emails/welcome.rb
+++ _emails/welcome.rb
@@ -1,2 +1,13 @@
-Welcome
+Subject: Welcome to Horizon
 
+Body: 
+Welcome to Horizon! When arriving in a new city, you likely desire to meet a friendly face; you want community. Staying with a friend or trusted connection is the most effective way to experience community while traveling. We built Horizon for those who place more importance on the connection and friendship, than on the details of accommodation.
+
+We believe life's magical moments don't happen staring at your screen, and the power of community to change lives.
+
+If you have questions or suggestions, please send them my way.
+
+Drew Meyers
+Co-Founder & CEO
+
+PS: If you're serious about using Horizon to travel or host, we ask that you visit Settings (via the ☰ button) and fill out the "Where Can You Host Guests" and "What Can You Offer Guests" fields.
